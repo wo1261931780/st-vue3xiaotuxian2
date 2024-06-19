22392f742ef6bab00d39a2891cfb258c87f5ab42
junw <wo1261931780@gmail.com>
2024-06-19 22:00:41 +0800

    ui 和插件导入

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// 其实只是完成了联想提示，而不是路径转换
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+	<el-button type="primary">Primary</el-button>
+</template>
+
+<style scoped>
+</style>
